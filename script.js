74c33db5cc658a1ee49a41459474d679f8a7cd64
DevanshM1211 <hem.devansh@gmail.com>
2024-06-25 13:18:39 +0100

    start  on google analytics

---
+++ script.js
@@ -0,0 +1,5 @@
+window.addEventListener("load", () => {
+    document.querySelector("head").insertAdjacentHTML("beforeend",
+        '<script async src="https://www.googletagmanager.com/gtag/js?id=G-TNDBXRERE6"></script><script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments)};gtag("js", new Date());gtag("config", "G-TNDBXRERE6");</script>'
+    )
+})
